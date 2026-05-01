990b82d1311ddfe197105f4bdcdb4446ebc1847b
Akhil <manojakhil36@gmail.com>
2026-05-01 20:46:20 +0530

    setup the client folder clean before start

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,7 @@
 function App() {
   return (
     <>
-      <h1 className='text-5xl font-extrabold text-red-400-600'>Hello</h1>
-      <h1 className='text-5xl font-extrabold text-red-400-600'>Hello</h1>
+   
     </>
   )
 }
