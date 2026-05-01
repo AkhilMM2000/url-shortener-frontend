095d14aa131608f77feeae7f0c56f9009f553954
Akhil <manojakhil36@gmail.com>
2026-05-01 19:45:21 +0530

    chore(frontend): initial react setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+    <>
+      <h1 className='text-5xl font-extrabold text-red-400-600'>Hello</h1>
+      <h1 className='text-5xl font-extrabold text-red-400-600'>Hello</h1>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// Dummy config file to trigger Tailwind CSS IntelliSense in VS Code monorepo
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
